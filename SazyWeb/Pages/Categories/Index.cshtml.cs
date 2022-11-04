c9f3e8da844cd79b00b5dce691e46e71e49f36ce
himanshuparmar121 <parmarhimanshu360@gmail.com>
2022-11-04 20:42:14 +0530

    Added changes for category display

---
+++ SazyWeb/Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SazyWeb.Data;
+using SazyWeb.Models;
+
+namespace SazyWeb.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public IEnumerable<Category> Categories { get; set; }
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void OnGet()
+        {
+            Categories = _db.Category;
+        }
+    }
+}

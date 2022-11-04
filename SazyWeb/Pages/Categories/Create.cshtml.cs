573dde94dbb28f456f4132fc214390360523d521
himanshuparmar121 <parmarhimanshu360@gmail.com>
2022-11-04 20:59:26 +0530

    Added create category functionality.

---
+++ SazyWeb/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SazyWeb.Data;
+using SazyWeb.Models;
+
+namespace SazyWeb.Pages.Categories
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        [BindProperty]
+        public Category Category { get; set; }
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            await _db.Category.AddAsync(Category);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("Index"); 
+        }
+    }
+}

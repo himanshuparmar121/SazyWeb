d79716ea81a50c82aac5e0fc5ddb3271873f913e
himanshuparmar121 <parmarhimanshu360@gmail.com>
2022-11-07 19:56:16 +0530

    Added server side validations.

--- SazyWeb/Pages/Categories/Create.cshtml.cs
+++ SazyWeb/Pages/Categories/Create.cshtml.cs
@@ -26,9 +26,13 @@ namespace SazyWeb.Pages.Categories
 
         public async Task<IActionResult> OnPost()
         {
-            await _db.Category.AddAsync(Category);
-            await _db.SaveChangesAsync();
-            return RedirectToPage("Index"); 
+            if (ModelState.IsValid)
+            {
+                await _db.Category.AddAsync(Category);
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            return Page();
         }
     }
 }
